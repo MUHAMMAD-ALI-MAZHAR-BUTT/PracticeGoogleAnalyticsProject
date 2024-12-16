8705ca6a6ddb521bc295d529d426bcee9279da60
Badar Shafiq <badar@predictview.com>
2024-12-16 22:19:48 +0500

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter as Router,
-  Routes,
-  Route,
+
   Link
 } from 'react-router-dom';
 import './index.css';
@@ -32,64 +31,6 @@ function Footer() {
   );
 }
 
-function Home() {
-  return (
-    <main>
-      <h1>Welcome to Our Website</h1>
-      <p>This is the homepage.</p>
-    </main>
-  );
-}
-
-function About() {
-  return (
-    <main>
-      <h1>About Us</h1>
-      <p>Learn more about us on this page.</p>
-    </main>
-  );
-}
-
-function Contact() {
-  return (
-    <main>
-      <h1>Contact Us</h1>
-      <p>Reach out to us via this page.</p>
-    </main>
-  );
-}
-
-function SignIn() {
-
-
-  useEffect(() => {
-    let environment = 'development'; // Default to local
-
-    // Push the environment to the dataLayer
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      'environment': environment,
-    });
-  }, []);
-  
-  
-  return (
-    <main>
-      <h1>Sign In</h1>
-      <>
-        <label>
-          Username:
-          <input type="text" name="username" required />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" required />
-        </label>
-        <button type="submit" id='signInButtonClickID'>Sign In</button>
-      </>
-    </main>
-  );
-}
 
 export default function App() {
 
